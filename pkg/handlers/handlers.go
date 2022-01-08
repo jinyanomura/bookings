9ce95cb09da2927c09ed17fe2027e350c062a1ab
Jinya Nomura <jinyanomura@gmail.com>
2022-01-08 22:06:16 +0900

    dealt with additional pages

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -21,20 +21,40 @@ func NewHandler(a *config.AppConfig) {
 }
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
+	// remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
 
-	stringMap := make(map[string]string)
-	stringMap["test"] = "Hello there!"
-	stringMap["remote_ip"] = remoteIp
+	// stringMap := make(map[string]string)
+	// stringMap["test"] = "Hello there!"
+	// stringMap["remote_ip"] = remoteIp
 	render.RenderTemplate(w, "home.page.html", &models.TemplateData{
-		StringMap: stringMap,
+		// StringMap: stringMap,
 	})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	// remoteIP := r.RemoteAddr
+	// m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
 	render.RenderTemplate(w, "about.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Searach(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.html", &models.TemplateData{})
 }
 
